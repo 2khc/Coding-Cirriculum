77c928a8b396a741cb8400b09c2002fccfc6b060
2khc <kh.kelvinchan@gmail.com>
2016-02-03 17:30:52 +0000

    Calculating n -factorial

--- game.c
+++ game.c
@@ -6,6 +6,7 @@
 // program terminates after operation complete.
 int basicAddition1()
 {
+    // need to give a description of all primitive datatypes here to the student.
     int numOne;
     int numTwo;
 
@@ -73,8 +74,39 @@ int flexibleCalculator()
     {
         result = numOne / numTwo;
     }
+
+    // There is an alternative to using so many if-else statements in a row.
+    // We call it switch-case statements.
+    // Thomas --> explain about switch-case and ask them to rewrite this if statement in a switch
+    // case format to achieve the same functionality.
+
     printf("%i %c %i = %i", numOne, operation, numTwo, result);
 }
+
+// how to demonstrate a for loop in a calculator setting?
+
+// how about if we wanted to calculate the n-factorial of a number?
+// we also introduce the concept of passing arguments into a function here.
+int nFactorial(int num)
+{
+    // as per usual, we first initialise the variables we will use to store our values;
+    // explain the mathematics of n!, ie n! = n * (n-1) * (n-2) * ...
+
+    int sum = num;  // we first set sum = num, why?
+
+    // To solve this problem we will use a for loop.
+    int i; // this is the counter for our for loop
+
+    for (i = 1; i < sizeof(num); i++) // explain what this does Thomas
+    {
+        sum = sum * (num - i); // parenthesis work just like they do in maths!
+    }
+    printf("%i\n", sum);
+
+}
+
+// Do it yourself exercise: It is possible to calculate the power of a number using for loops.
+// how would you do this? try it now!
 
 
 int main(void)
@@ -82,5 +114,6 @@ int main(void)
 
 
 //    basicAddition1();
-    flexibleCalculator();
+//    flexibleCalculator();
+    nFactorial(5);
 }
